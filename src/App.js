98c703f45a5c38e14adabe2d673e8260678f18d3
michaellu2019 <mlu0708@mit.edu>
2021-01-12 17:41:14 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="primary">
+        Toppings
+      </header>
+    </div>
+  );
+}
+
+export default App;
