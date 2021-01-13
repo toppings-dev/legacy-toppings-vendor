476606e6ab4bdbfea59603f8e99aefaf72307ad0
michaellu2019 <mlu0708@mit.edu>
2021-01-13 02:12:25 -0500

    Added vendor portal routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Portal from './pages/Portal';
 
 function App() {
   return (
     <div className="wrapper">
+      <BrowserRouter>
+        <Route exact path="/portal" component={Portal} />
+      </BrowserRouter>
+
       <header className="primary">
         Toppings
       </header>
---
+++ src/pages/Portal.js
@@ -0,0 +1,13 @@
+
+
+function Portal() {
+  return (
+    <div className="portal-container">
+      <h1>
+        Toppings Vendor Portal
+      </h1>
+    </div>
+  );
+}
+
+export default Portal;
