adb0f74d2095f5765bc2621552293c278e1dac96
michaellu2019 <mlu0708@mit.edu>
2021-01-13 14:13:26 -0500

    Fixed vendor portal login page bugs

--- src/pages/Portal.js
+++ src/pages/Portal.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 function Portal() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
 
-  function login() {
-    setError(prevError => !prevError);
-    console.log(error);
+  function login(e) {
+    e.preventDefault();
+    console.log(error, email, password);
   }
 
   return (
@@ -17,11 +20,11 @@ function Portal() {
           Toppings Vendor Portal
         </h1>
 
-        <form>
+        <form onSubmit={login}>
           {error ? <span className="error-message">Incorrect username or password.</span> : ""}
           <label for="email">Email Address</label><input className="text-input" type="email" onChange={(e) => setEmail(e.target.value)} />
           <label for="password">Password</label><input className="text-input" type="password" onChange={(e) => setPassword(e.target.value)} />
-          <input className="submit-button" type="submit" value="Submit" onClick={login} />
+          <input className="submit-button" type="submit" value="Submit" />
         </form>
       </div>
     </div>
